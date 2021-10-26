d7f06e4b8fe98132d4f6ebc906e388561af7feb8
Okinka <olenkakinka1995@gmail.com>
2021-10-26 11:31:22 +0200

    Task complete

--- src/it/campe/Main.java
+++ src/it/campe/Main.java
@@ -5,7 +5,16 @@ public class Main {
     public static void main(String[] args) {
         int toothCounter = 32;
 
-        //напишите тут ваш код
+        toothCounter--;
+        toothCounter--;
+        toothCounter--;
+        toothCounter--;
+        toothCounter--;
+        toothCounter--;
+        toothCounter--;
+        toothCounter--;
+        toothCounter--;
+
 
 
         System.out.println(toothCounter);
